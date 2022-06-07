7c844ba14b26e3d001d039f2621289ea9bf62ac2
Khaz <avatar.rafi@gmail.com>
2022-06-07 21:28:45 +0700

    initial commit

---
+++ app/src/main/java/com/rafi/uts10119079_dailynotes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rafi.uts10119079_dailynotes;
+//07 Juni 2022, 10119079, Rafi Khalifanan Azanan, IF2
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
